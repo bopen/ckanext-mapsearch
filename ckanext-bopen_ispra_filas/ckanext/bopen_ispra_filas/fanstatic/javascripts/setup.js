92a4ebe4ad7308e77e91de8df54d46294a93dc01
Christian Wörner <christianworner@gmail.com>
2014-01-14 08:51:08 +0100

    make map full-height

--- ckanext-bopen_ispra_filas/ckanext/bopen_ispra_filas/fanstatic/javascripts/setup.js
+++ ckanext-bopen_ispra_filas/ckanext/bopen_ispra_filas/fanstatic/javascripts/setup.js
@@ -1,2 +1,16 @@
-alert("erre")
-console.log($)
+bop = {};
+
+$(document).ready(function () {
+    bop.resizeDiv();
+});
+
+window.onresize = function (event) {
+    bop.resizeDiv();
+}
+
+bop.resizeDiv =  function () {
+    bop.x_full = $(window).width();
+    bop.y_full = $(window).height();
+    $('#all').css({'height': bop.y_full + 'px'});
+    $('#map-container').css({'height':  bop.y_full + 'px'});
+}
