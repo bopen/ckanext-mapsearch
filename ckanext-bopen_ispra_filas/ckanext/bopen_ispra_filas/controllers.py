fd5cd07654c470791366104c2f67367a852a53de
Christian Wörner <christianworner@gmail.com>
2014-01-15 18:11:31 +0100

    adds controller with 2 actions

--- ckanext-bopen_ispra_filas/ckanext/bopen_ispra_filas/controllers.py
+++ ckanext-bopen_ispra_filas/ckanext/bopen_ispra_filas/controllers.py
@@ -1,10 +1,36 @@
+import json
 import ckan.plugins as plugins
-from ckan.lib.base import BaseController
+from ckan.lib.base import BaseController, request
+from ckanext.spatial.lib import bbox_query,validate_bbox
 
 #import logging
 #log = logging.getLogger(__name__)
 
 class ViewController(BaseController):
-
     def show(self):
         return plugins.toolkit.render('blank_search_app.html')
+
+    def text_complete(self):
+        """must autocomplete take the map-extent in consideration?"""
+        text_results, geo_results = self._do_query(request)
+        #print "geo:", geo_results["results"]
+        #print "text:", [r['id'] for r in text_results['results']]
+
+        return json.dumps([r["title"] for r in text_results['results'] if r['id'] in geo_results['results']])
+
+    def query_datasets(self):
+        """the full query returning datasets"""
+        text_results, geo_results = self._do_query(request)
+        return json.dumps([r for r in text_results['results'] if r['id'] in geo_results['results']])
+
+    def _do_query(self, request):
+        package_search = plugins.toolkit.get_action('package_search')
+        #print request.params.keys()
+        q = request.params["q"]
+        srid = get_srid(request.params.get('crs')) if 'crs' in request.params else None
+        extents = bbox_query(validate_bbox(request.params["bbox"]), srid)
+        ids = [extent.package_id for extent in extents]
+        geo_results = dict(count=len(ids),results=ids)
+        q_string = "title:*{0}* OR notes:*{0}*".format(q)
+        text_results = package_search(None, {'q': q_string})
+        return text_results, geo_results
