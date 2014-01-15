6b0a792c04968e47aeab4896971357a4812db6fd
Christian Wörner <christianworner@gmail.com>
2014-01-15 18:15:35 +0100

    registers 3 routes in the plugin

--- ckanext-bopen_ispra_filas/ckanext/bopen_ispra_filas/plugin.py
+++ ckanext-bopen_ispra_filas/ckanext/bopen_ispra_filas/plugin.py
@@ -17,6 +17,8 @@ class BopenIspraFilasPlugin(plugins.SingletonPlugin):
     ## IRoutes
     def before_map(self, map):
         controller = 'ckanext.bopen_ispra_filas.controllers:ViewController'
-        map.connect('map_viewer', '/viewer', controller=controller, action='show')
+        map.connect('map_viewer', '/map-search', controller=controller, action='show')
+        map.connect('map_viewer', '/map-search/text_complete', controller=controller, action='text_complete')
+        map.connect('map_viewer', '/map-search/query_datasets', controller=controller, action='query_datasets')
         #map.redirect('/', '/dataset')
         return map
