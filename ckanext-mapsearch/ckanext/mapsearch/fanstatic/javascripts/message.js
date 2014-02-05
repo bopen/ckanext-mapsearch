8606360c5b0d5baa0f013babca0036757964f5de
Christian Wörner <christianworner@gmail.com>
2014-02-05 11:20:58 +0100

    adds javascript notifications

---
+++ ckanext-mapsearch/ckanext/mapsearch/fanstatic/javascripts/message.js
@@ -0,0 +1,15 @@
+this.ckan.module('mapsearch-message', function ($, _) {
+    if (typeof bop === 'undefined') {bop = {};}
+    bop.display_message = function (msg) {
+      var cont = $('#mapsearch_flash'),
+          ele = $('#message_prototype').clone().show();
+      ele.attr('id', Number(new Date()));
+      ele.html(msg);
+      cont.append(ele);
+      setTimeout(function () {
+         ele.hide('slow', function () {
+            ele.remove();
+         });
+      }, 3000);
+    };
+});
