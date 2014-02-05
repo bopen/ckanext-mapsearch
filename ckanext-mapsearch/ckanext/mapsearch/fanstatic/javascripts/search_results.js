4993c07f4a762ed66d74064f86c6295c9a699830
Christian Wörner <christianworner@gmail.com>
2014-02-05 15:42:14 +0100

    render small extents toggle-able

--- ckanext-mapsearch/ckanext/mapsearch/fanstatic/javascripts/search_results.js
+++ ckanext-mapsearch/ckanext/mapsearch/fanstatic/javascripts/search_results.js
@@ -6,10 +6,30 @@ this.ckan.module('mapsearch-search_results', function ($, _) {
         bop.display_search_results();
         bop.update_filter_panel();
     };
+    bop.toggle_small_omitted = function () {
+      var ele = $('.omitted_small span');
+        if (bop.small_extents_are_displayed) {
+          bop.hide_small_omitted();
+          ele.removeClass("displayed");
+          bop.small_extents_are_displayed = false;
+        } else {
+          bop.show_small_omitted();
+          ele.addClass("displayed");
+          bop.small_extents_are_displayed = true;
+        }
+    };
     bop.show_small_omitted = function () {
         $.each(bop.omitted_results.small.results, function (idx, dataset) {
             bop.add_extent_to_map(dataset, {small: true});
         });
+    };
+    bop.hide_small_omitted = function () {
+      bop.result_layer.eachLayer(function (layer) {
+        var props = layer.feature.properties;
+        if (props.mapsearch && props.mapsearch == 'small') {
+            bop.result_layer.removeLayer(layer);
+        }
+      });
     };
     bop.display_search_results = function () {
         var idx = bop.result_nav.current_page,
@@ -50,7 +70,7 @@ this.ckan.module('mapsearch-search_results', function ($, _) {
                 if (options && options.small) {
                    geoJSON.properties.style = bop.too_small_to_display_style;
                    geoJSON.properties.mapsearch = 'small';
-                };
+                }
                 layer = bop.result_layer.addData(geoJSON);
             }
         }
